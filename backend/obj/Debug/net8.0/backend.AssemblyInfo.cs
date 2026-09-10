a6b8a6eb2da95c1dae0cce1029ac57f7e95bdb7b
makstuk1410 <makstuk1410@gmail.com>
2026-09-10 21:54:35 +0200

    Configure Docker and Docker Compose #5

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7069acb34e057243bf16451d01e5fddc7429abe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd8e664b88207557f8af94212452b7b560ee3c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
