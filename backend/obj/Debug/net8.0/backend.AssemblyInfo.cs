71ed959d91abd1b60a51eb972e9c5e28119a5632
makstuk1410 <makstuk1410@gmail.com>
2026-09-24 12:44:36 +0200

    corrected images

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc23d8307160b32dd214e2cba001bca2d5fafab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa33162c19f9f2a82750a3d12cd7068c9e5894b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
